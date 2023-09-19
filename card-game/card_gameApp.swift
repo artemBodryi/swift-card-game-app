6e05a002941cd9397df8885a85b802fbf64271ae
artemBodryi <artembodry@gmail.com>
2023-09-19 11:36:28 +0300

    Initial Commit

---
+++ card-game/card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  card_gameApp.swift
+//  card-game
+//
+//  Created by Артем Бодрый on 19/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
